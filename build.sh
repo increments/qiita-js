a643c7608e8fdd89581069f538ea9fd6bb9e740c
Koutarou Chikuba <miz404@gmail.com>
2014-11-14 17:58:28 +0900

    Use command via node_modules

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-schema-tsifier kitchen/schema.yml Qiita --with-resources > kitchen/qiita.ts
-tsc --target es5 -d kitchen/qiita.ts
+./node_modules/.bin/schema-tsifier kitchen/schema.yml Qiita --with-resources > kitchen/qiita.ts
+./node_modules/.bin/tsc --target es5 -d kitchen/qiita.ts
 cat kitchen/qiita.js environments/browser.js > dist/qiita-browser.js
 cat kitchen/qiita.js environments/commonjs.js > dist/qiita-commonjs.js
 cp kitchen/qiita.d.ts dist/
