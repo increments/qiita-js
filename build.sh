ea081c12a7c6daac1466516bd469bd29aa3fdd17
hina <hina@hinaloe.net>
2015-03-01 00:15:25 +0900

    standalone (able w/o module system)

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 ./node_modules/.bin/schema-tsifier kitchen/schema.yml Qiita --with-resources > kitchen/qiita.ts
 ./node_modules/.bin/tsc --target es5 -d kitchen/qiita.ts
-browserify -t coffeeify --extension=".coffee" src/index.coffee -o dist/qiita.js
+browserify -t coffeeify --extension=".coffee" src/index.coffee -o dist/qiita.js --standalone=Qiita
